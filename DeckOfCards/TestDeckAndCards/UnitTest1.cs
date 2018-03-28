144ece06f3a1319bc8302d1aac001e101409dbfa
wtnerb <wtnerb@gmail.com>
2018-03-28 09:24:34 -0700

    scaffold

---
+++ DeckOfCards/TestDeckAndCards/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using DeckOfCards;
+
+namespace TestDeckAndCards
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
